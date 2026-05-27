ecbab4cb4e7570891424da2b1c6d80b55ea4bc35
Sergey2025-od <2017obzor2017@gmail.com>
2026-05-27 20:29:27 +0300

    Create mono.js

---
+++ src/mono.js
@@ -0,0 +1,80 @@
+//
+// DISTRICT MATCH
+//
+let districtCode = null;
+
+if (
+  text.includes("черем") ||
+  text.includes("черемушки") ||
+  text.includes("черьому")
+) {
+  districtCode = "cheremushki";
+}
+
+if (
+  text.includes("молд")
+) {
+  districtCode = "moldovanka";
+}
+
+if (
+  text.includes("арк")
+) {
+  districtCode = "arkadia";
+}
+
+if (
+  text.includes("таир") ||
+  text.includes("таір") ||
+  text.includes("лиман") ||
+  text.includes("сав")
+) {
+  districtCode = "tairchik";
+}
+
+if (
+  text.includes("центр")
+) {
+  districtCode = "center";
+}
+
+if (
+  text.includes("слобод")
+) {
+  districtCode = "slobodka";
+}
+
+if (
+  text.includes("перес")
+) {
+  districtCode = "peresyp";
+}
+
+if (
+  text.includes("поскот")
+) {
+  districtCode = "poskot";
+}
+
+if (
+  text.includes("аванг") ||
+  text.includes("7 км") ||
+  text.includes("ленпас")
+) {
+  districtCode = "avangard";
+}
+
+if (
+  text.includes("крива") ||
+  text.includes("усат") ||
+  text.includes("неруб")
+) {
+  districtCode = "krivaya";
+}
+
+if (
+  text.includes("холод") ||
+  text.includes("дачн")
+) {
+  districtCode = "holodka";
+}
