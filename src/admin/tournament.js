33db6754ebe55502da11c41cc15c8da544a8520d
Sergey2025-od <2017obzor2017@gmail.com>
2026-05-28 23:49:44 +0300

    Update tournament.js

--- src/admin/tournament.js
+++ src/admin/tournament.js
@@ -265,7 +265,7 @@ module.exports = (
                           "🗳 ПРОГОЛОСУВАТИ",
 
                         url:
-                          "https://t.me/bitva_rayoniv_bot"
+                          "https://t.me/bitva_rayoniv_bot?start=vote"
                       }
                     ]
                   ]
