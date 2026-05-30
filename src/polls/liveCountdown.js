132f14fd13e1385ea96f1ac3c7149e4ac53dbc84
Sergey2025-od <2017obzor2017@gmail.com>
2026-05-30 10:36:32 +0300

    Update liveCountdown.js

--- src/polls/liveCountdown.js
+++ src/polls/liveCountdown.js
@@ -65,8 +65,20 @@ if (
   poll.tournament_stage
 ) {
 
-  text +=
-    `🏁 ${poll.tournament_stage} фіналу\n\n`;
+  if (
+    poll.tournament_stage
+      .toLowerCase() ===
+    "фінал"
+  ) {
+
+    text +=
+      `🏁 Фінал\n\n`;
+
+  } else {
+
+    text +=
+      `🏁 ${poll.tournament_stage} фіналу\n\n`;
+  }
 }
 
     if (
